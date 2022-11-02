6be07b8644e71140cf602585cb59e59257b78e63
Kristian Vassilev <kristian.vassilev@mentormate.com>
2022-11-02 15:28:16 +0200

    Added Counter Demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -23,6 +23,12 @@ const HomeScreen = ({ navigation }) => {
           navigation.navigate("Image");
         }}
       />
+      <Button
+        title="Go to Counter Demo"
+        onPress={() => {
+          navigation.navigate("Counter");
+        }}
+      />
     </View>
   );
 };
