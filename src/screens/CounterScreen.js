f5f8902249a0beed2183e00f85cc0d203fabaaaa
Kristian Vassilev <kristian.vassilev@mentormate.com>
2022-11-08 13:45:29 +0200

    Changed counter state logic to reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  // state = { count: number};
+  // action === {type: 'change_count', payload: 1 || -1 }
+
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
+
   return (
     <View>
       <Button
         title="Increase"
-        onPress={() => {
-          setCounter((prev) => prev + 1);
-        }}
+        onPress={() => dispatch({ type: "increment", payload: 1 })}
       />
       <Button
         title="Decrease"
-        onPress={() => {
-          setCounter((prev) => prev - 1);
-        }}
+        onPress={() => dispatch({ type: "decrement", payload: 1 })}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {count}</Text>
     </View>
   );
 };
